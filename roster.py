1fd9896904048919e370d64cc5574a9b04a437db
ajaholman <ajarieholman@gmail.com>
2023-12-13 23:53:22 -0500

    more players rounded bmi

--- roster.py
+++ roster.py
@@ -2,10 +2,16 @@
 
 import pandas as pd
 
-player = {"Last Name": ["Bacot", "Davis", "Cadeau"],
-          "First Name": ["Armando", "RJ", "Elliot"],
-          "height": [83, 72, 73],
-          "weight": [240, 180, 180]}
+player = {"Last Name": ["Bacot", "Davis", "Cadeau", "High", "Trimble", "Wojcik", "Washington", "Lebo", "Landry", "Withers", "Okonkwo", "Farris", "Ingram"],
+          "First Name": ["Armando", "RJ", "Elliot", "Zayden", "Seth", "Paxson", "Jalen", "Creighton", "Rob", "Jae'Lyn", "James", "Duwe", "Harrison"],
+          "height": [83, 72, 73, 81, 75, 77, 82, 73, 76, 81, 80, 79, 79],
+          "weight": [240, 180, 180, 225, 195, 195, 230, 180, 190, 215, 240, 210, 235]
+}
 
 data = pd.DataFrame(player)
+
+data["bmi"] = (data["weight"]/2.205)/((data["height"]/39.37)**2)
+data['rounded_bmi'] = data["bmi"].round(2)
 print(data)
+
+data.to_csv("bmi.csv")
